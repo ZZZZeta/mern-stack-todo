b89f2b7dbf993cce10689ccf00aa882cefa7a7e9
ZZZZeta <fastmuvie@gmail.com>
2019-10-06 12:01:40 +0300

    initial commit

---
+++ src/components/todos-list.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodosList extends Component {
+    
+    render() { 
+        return ( 
+            <div>
+                <p>Welcome to Todos List Component!</p>
+            </div>
+         );
+    }
+}
+ 
+export default TodosList;
